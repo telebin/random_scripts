a36634f77ab7a77e7fd1b5777c69df2f0faf3fa0
Adrian Sitko <adriansitko@gmail.com>
2020-12-07 21:18:03 +0100

    Added proper handler for SJP, refactored and cleaned

--- suggestionTranslator/suggestionTranslator.hs
+++ suggestionTranslator/suggestionTranslator.hs
@@ -16,19 +16,27 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Text.JSON as Json
 
-parseDiki :: Json.Result Json.JSValue -> [String]
+type Translator = Json.Result Json.JSValue -> [String]
+
+parseDiki :: Translator
 parseDiki (Json.Error x) = [x]
-parseDiki (Json.Ok (Json.JSArray strs)) = map conv strs where
+parseDiki (Json.Ok (Json.JSArray strs)) = map conv strs
+  where
     conv (Json.JSString str) = Json.fromJSString str
     conv _ = "Just not expected"
 parseDiki _ = ["Invalid JSON format"]
 
+parseSjp :: Translator
+parseSjp (Json.Error x) = [x]
+parseSjp (Json.Ok (Json.JSArray objs)) = map (conv . unpack) objs
+  where
+    unpack (Json.JSObject obj) = let [_, value, _] = Json.fromJSObject obj in snd value
+    conv (Json.JSString str) = Json.fromJSString str
+    conv _ = "Just not expected"
+parseSjp _ = ["Invalid JSON format"]
+
 prepareAnswer :: String -> [String] -> String
 prepareAnswer q ans = Json.encode (q, ans)
-
-dikiAnsw = "[\"hen\",\"Henryk\",\"Henry\",\"Henryka\",\"hence\",\"Henrietta\",\"henceforth\",\"henchman\",\"henna\",\"henhouse\",\"Henoch\",\"Henize 3-1357\",\"hendiadys\",\"henogamia\",\"henoteistyczny\",\"henoteizm\",\"hentai\",\"heneken\",\"Henle's layer\",\"Henle's loop\",\"Henriad\",\"Henrician Articles\",\"Henry rifle\",\"Hensen's cell\",\"H\\u00e9non map\",\"hen harrier\",\"hen run\",\"henbane\",\"henbit\",\"henbit dead-nettle\",\"hench\",\"henchperson\",\"henchwoman\",\"hendecagon\",\"hendecagonal\"]"
-sjpAnsw = "[{\"label\":\"<b>pen<\\/b>akordancja\",\"value\":\"penakordancja\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>alista\",\"value\":\"penalista\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>alistka\",\"value\":\"penalistka\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>alizacja\",\"value\":\"penalizacja\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>alny\",\"value\":\"penalny\",\"language\":[\"sjp\"]},{\"label\":\"<b>Pen<\\/b>ang\",\"value\":\"Penang\",\"language\":[\"sjp\"]},{\"label\":\"<b>Pen<\\/b>club\",\"value\":\"Penclub\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>del\",\"value\":\"pendel\",\"language\":[\"sjp\"]},{\"label\":\"<b>Pen<\\/b>derecki\",\"value\":\"Penderecki\",\"language\":[\"sjp\"]},{\"label\":\"<b>pen<\\/b>dlowa\\u0107\",\"value\":\"pendlowa\\u0107\",\"language\":[\"sjp\"]}]"
-fdAnsw = "SAYT.Callback([\"mend\", [[\"mend\",3125],[\"mend (one's) fences\",1024],[\"mend (one's) pace\",1024],[\"mend (one's) ways\",1024],[\"mend bones\",2061],[\"mend fences\",1024],[\"mend her fences\",1024],[\"mend her pace\",1024],[\"mend her ways\",1024],[\"mend his fences\",1024]]])"
 
 get :: Manager -> String -> IO BS.ByteString
 get manager address = do
@@ -44,26 +52,22 @@ main = do
 suggestionTranslatorRouter :: Manager -> Snap ()
 suggestionTranslatorRouter manager =
     route [ ("di/:searchString", dikiHandler manager)
-          , ("sjp/:searchString", sjpHandler)
-          , ("fd/:searchString", fdHandler)
+          , ("sjp/:searchString", sjpHandler manager)
           ]
 
 dikiHandler :: Manager -> Snap ()
-dikiHandler manager = do
+dikiHandler = translateRequest "https://www.diki.pl/dictionary/autocomplete?langpair=en%3A%3Apl&q=" parseDiki
+
+sjpHandler :: Manager -> Snap ()
+sjpHandler = translateRequest "https://sjp.pwn.pl/complete.php?source=autocomplete-sjp&query=" parseSjp
+
+translateRequest :: String -> Translator -> Manager -> Snap ()
+translateRequest address translate manager = do
     param <- getParam "searchString"
     if isJust param then do
         let toSearch = fromJust param
-            address = "https://www.diki.pl/dictionary/autocomplete?langpair=en%3A%3Apl&q=" ++ BSC.unpack toSearch
-        res <- liftIO $ get manager address
-        writeBS $ BSC.pack $ prepareAnswer (BSC.unpack toSearch) (parseDiki $ Json.decode $ BSC.unpack res)
+        res <- liftIO $ get manager (address ++ BSC.unpack toSearch)
+        logError res
+        writeBS $ BSC.pack $ prepareAnswer (BSC.unpack toSearch) (translate $ Json.decode $ BSC.unpack res)
     else writeBS "searchString parameter missing"
 
-sjpHandler :: Snap ()
-sjpHandler = do
-    param <- getParam "searchString"
-    writeBS (fromJust param)
-
-fdHandler :: Snap ()
-fdHandler = do
-    param <- getParam "searchString"
-    writeBS (fromJust param)
